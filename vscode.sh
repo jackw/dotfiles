c78db5ef4529327858de5f32d6a7ad0718bbc5b9
Jack Westbrook <jack.westbrook@gmail.com>
2019-12-19 12:00:27 +0100

    setup vscode

--- vscode.sh
+++ vscode.sh
@@ -5,6 +5,6 @@ mkdir -p "$HOME/Library/Application Support/Code/User/"
 ln -sfn $(pwd)/vscodeFiles/settings.json "$HOME/Library/Application Support/Code/User/settings.json"
 
 # Install wanted extensions
-cat vscodeFiles/extensions | xargs -L 1 echo code --install-extension
+cat vscodeFiles/extensions | xargs -L 1 code --install-extension
 
 #TODO: get animals of paradise without copying from elsewhere.
