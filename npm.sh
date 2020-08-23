e2fc061970d7be336fd21fc57c011846965ed642
Jack Westbrook <jack.westbrook@gmail.com>
2020-08-23 18:51:00 +0200

    feat(nvm): install latest, stable and v10. add default-packages

--- npm.sh
+++ npm.sh
@@ -1,15 +1,7 @@
-# Install LTS
-nvm install --lts
-
-# Install commonly used clis
-npm i -g commitizen
-npm i -g cz-conventional-changelog
-npm i -g http-server
-npm i -g elfinder
-npm i -g svgo
-
-# Let's drop node 10 here also and make sure the global packages are available
-nvm install lts/dubnium --reinstall-packages-from=node
+# Install versions of node
+nvm install node # latest
+nvm install --lts # latest stable
+nvm install lts/dubnium # node 10
 
 # Default commitizen to use conventional changelog
 echo '{ "path": "cz-conventional-changelog" }' > ~/.czrc
